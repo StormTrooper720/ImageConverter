2cd780013dc7253286039fb2484f88f2a4243888
StormTrooper720 <78572300+StormTrooper720@users.noreply.github.com>
2021-02-04 19:03:45 -0700

    Update convert_images.py

--- convert_images.py
+++ convert_images.py
@@ -1,11 +1,14 @@
+# Import all the libraries if you haven't already
 from tkinter import *
 import tkinter as tk
 from PIL import Image
 from tkinter import filedialog
+# Defining some variables so we don't get errors
 img = None
 text = "Image Converter In Python"
 
 
+# Function for opening a file dialogue box to select a file
 def file_open():
     global img
     filename = filedialog.askopenfilename(initialdir="/", title="Open An Image", filetypes=(
@@ -19,12 +22,14 @@ def file_open():
     label_file_explorer.configure(text="File Opened: " + filename)
 
 
+# Opening another file dialogue window to save the converted image
 def file_save():
     global img
     global label_file_explorer
     save_file = filedialog.asksaveasfilename(initialdir="/", title="Save The Image", filetypes=(
         ("Image Files", "*.png*"), ("Image Files", "*.jpg*"), ("Image Files", "*.jpeg*"), ("Image Files", "*.ico*"),
         ("Image Files", "*.tif*"), ("Image Files", "*.tiff*"), ("Image Files", "*.jp2*"), ("Image Files", "*.bmp*")))
+    # Excepting all the possible errors
     try:
         img.save(save_file)
         label_file_explorer.configure(text="Image Saved As: " + save_file)
@@ -39,15 +44,16 @@ def file_save():
         label_file_explorer.configure(text="You must use a valid image type")
 
 
+# Seting up the tkinter window
 window = Tk()
 window.title('Image Converter')
 window.geometry("707x150")
 window.config(background="white")
 label_file_explorer = Label(window, text=text, width=100, height=4, fg="blue")
-
+# Creating the buttons the open and save a file
 button_open = Button(window, text="Open File", command=file_open)
 button_convert = Button(window, text="Convert File", command=file_save)
-
+# Placing the buttons in a grid within the window
 label_file_explorer.grid(column=1, row=1)
 button_open.grid(column=1, row=2)
 button_convert.grid(column=1, row=3)
